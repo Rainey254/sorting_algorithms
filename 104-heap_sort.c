d32c6540974a866867a84c7f39f3d689abffb9b0
rainey254 <reinykenya@gmail.com>
2023-11-18 07:27:29 -0800

    anything

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,87 +1,84 @@
 #include "sort.h"
 #include <stdio.h>
-
 /**
- * swap - utility function to swap to integers
- * @a: integer a
- * @b: integer b
- **/
-void swap(int *a, int *b)
+ * swap_nums - swaps numbers
+ *
+ * @arr: input array
+ * @a: first index
+ * @b: second index
+ * Return: no return
+ */
+void swap_nums(int *arr, int a, int b)
 {
-	int t = *a;
-
-	*a = *b;
-	*b = t;
+	arr[a] = arr[a] + arr[b];
+	arr[b] = arr[a] - arr[b];
+	arr[a] = arr[a] - arr[b];
 }
 
 /**
- * maxHeapify - The main function to heapify a Max Heap. The function
- * assumes that everything under given root (element at index idx)
- * is already heapified
- * @array: array
- * @size: size of the array for print
- * @idx: index
- * @n: size of the array to run
+ * recursion_heap - recursion that builds the max heap tree
+ *
+ * @arr: input array
+ * @i: index number
+ * @size: size of the array
+ * @limit: limit of the array
+ * Return: no return
  */
-void maxHeapify(int *array, size_t size, int idx, size_t n)
+void recursion_heap(int *arr, int i, size_t size, int limit)
 {
-	int largest = idx;		 /* Initialize largest as root*/
-	int left = 2 * idx + 1;	 /* left = (idx << 1) + 1*/
-	int right = 2 * idx + 2; /* right = (idx + 1) << 1*/
+	int bigger;
+	int i2;
 
-	/* See if left child of root exists and is greater than root*/
-	if (left < (int)n && array[left] > array[largest])
-		largest = left;
+	i2 = i * 2;
 
-	/**
-	 * See if right child of root exists and is greater than
-     *the largest so far
-	 */
-	if (right < (int)n && array[right] > array[largest])
-		largest = right;
-
-	/* Change root, if needed*/
-	if (largest != idx)
+	if (i2 + 2 < limit)
 	{
-		swap(&array[idx], &array[largest]);
-		print_array(array, size);
-		maxHeapify(array, size, largest, n);
+		recursion_heap(arr, i2 + 1, size, limit);
+		recursion_heap(arr, i2 + 2, size, limit);
+	}
+
+	if (i2 + 1 >= limit)
+		return;
+
+	if (i2 + 2 < limit)
+		bigger = (arr[i2 + 1] > arr[i2 + 2]) ? (i2 + 1) : (i2 + 2);
+	else
+		bigger = i2 + 1;
+
+	if (arr[i] < arr[bigger])
+	{
+		swap_nums(arr, i, bigger);
+		print_array(arr, size);
+		recursion_heap(arr, bigger, size, limit);
 	}
 }
 
 /**
- * heap_sort -  The main function to sort an array of given size
- * @array: array to sort
+ * heap_sort - sorts an array of integers in ascending
+ * order using the Heap sort algorithm
+ *
+ * @array: input array
  * @size: size of the array
- **/
+ */
 void heap_sort(int *array, size_t size)
 {
 	int i;
-	/**
-	 * Start from bottommost and rightmost internal mode and heapify all
-     * internal modes in bottom up way
-	 */
-	if (array == '\0' || size < 2)
+	size_t limit;
+
+	if (!array || size == 0)
 		return;
 
-	for (i = (size - 2) / 2; i >= 0; --i)
-		maxHeapify(array, size, i, size);
+	i = 0;
+	limit = size;
 
-	/**
-	* Repeat following steps while heap size is greater than 1.
-    * The last element in max heap will be the minimum element
-	*/
-	for (i = (size - 1); i > 0; --i)
+	while (limit > 1)
 	{
-		/**
-		* The largest item in Heap is stored at the root. Replace
-		*it with the last item of the heap followed by reducing the
-		*size of heap by 1.
-		*/
-		swap(&array[0], &array[i]);
-		print_array(array, size);
-
-		/* Finally, heapify the root of tree.*/
-		maxHeapify(array, size, 0, i);
+		recursion_heap(array, i, size, limit);
+		if (array[i] >= array[limit - 1])
+		{
+			swap_nums(array, i, limit - 1);
+			print_array(array, size);
+		}
+		limit--;
 	}
 }
